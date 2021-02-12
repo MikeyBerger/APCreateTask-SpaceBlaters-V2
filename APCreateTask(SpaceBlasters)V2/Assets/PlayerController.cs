ea594e87705e3aa838d37d8955ac91d55a075ee9
MikeyBerger <michaeljudahberger@gmail.com>
2021-02-11 16:14:00 -0800

    Initial Commit

---
+++ APCreateTask(SpaceBlasters)V2/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D RB;
+    private Vector2 Movement;
+    public float Speed;
+    public bool IsShooting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        RB.velocity = new Vector2(Movement.x, Movement.y) * Speed * Time.deltaTime;
+        //transform.Translate(Movement.x * Speed, Movement.y * Speed, 0);
+    }
+
+
+    #region InputActions
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Movement = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnShoot(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase == InputActionPhase.Performed)
+        {
+            IsShooting = true;
+        }
+    }
+    #endregion
+}
